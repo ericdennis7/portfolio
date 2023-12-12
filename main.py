642311ff3ff339873b44f8d607275d3e0512b8fb
ericdennis7 <130507070+ericdennis7@users.noreply.github.com>
2023-12-12 15:13:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -53,8 +53,8 @@ if selected == "Home":
                 ''')
 
     cover = [
-        dict(title="", text="", interval=None, img="https://www.thesimplebeliever.com/wp-content/uploads/2023/12/image-e1702411892625.jpg"),
-        dict(title="", text="", img="https://www.thesimplebeliever.com/wp-content/uploads/2023/12/IMG20220823182117-scaled.jpg"),
+        dict(title="Eric and Simon", text="", interval=None, img="https://www.thesimplebeliever.com/wp-content/uploads/2023/12/image-e1702411892625.jpg"),
+        dict(title="Mountain Biking w/ Deer", text="", img="https://www.thesimplebeliever.com/wp-content/uploads/2023/12/IMG20220823182117-scaled.jpg"),
 ]
     carousel(items=cover)
 
